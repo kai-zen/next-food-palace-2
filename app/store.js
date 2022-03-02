9b3dffda65169a6a7b0a69c2bcf5e1828c4843f2
kai-zen <razipurali@gmail.com>
2022-03-02 16:08:00 +0330

    Used cookies to save switched theme mode

--- app/store.js
+++ app/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from './../features/themeSlice'
 import foodsReducer from './../features/foodsSlice'
 import usersReducer from './../features/usersSlice'
-import commentsReducer from './../features/commentsSlice'
 
 export const store = configureStore({
     reducer: {
-        theme: themeReducer,
         foods: foodsReducer,
         users: usersReducer,
-        comments: commentsReducer,
     },
 })
